cbe1103ba5640a6cc498bbd72fad9de0b7bd1c77
TerranCivilian <63529094+TerranCivilian@users.noreply.github.com>
2022-06-10 22:22:33 -0400

    clean this up a bit

--- src/App.js
+++ src/App.js
@@ -1,51 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class List extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: [1,2,3],
-    };
-  }
-
-  render() {
-    let renderedList = this.state.list.map((i) =>
-      <li>{i}</li>
-    );
-
-    return (
-      <div>
-        <span>
-          <button
-            onClick={() => {
-              const nums = this.state.list.slice();
-              nums.push(this.state.list.length + 1);
-              this.setState({list: nums});
-            }}>
-            +
-          </button>
-          <button
-            onClick={() => {
-              if (this.state.list.length > 0) {
-                const nums = this.state.list.slice();
-                nums.pop();
-                this.setState({list: nums});
-              };
-            }}>
-            -
-          </button>
-        </span>
-        <ul>
-        {renderedList}
-        </ul>
-      </div>
-    );
-  }
+function renderedList(list) {
+  return (
+    list.map(i => <li>{i}</li>)
+  )
 }
 
 function App() {
+  const [list, setList] = useState([1,2,3]);
+
   return (
-      <List />
+    <div>
+      <span>
+        <button onClick={() => list.length > 0 ? setList([...list, list[list.length-1] + 1]) : setList([1])}>
+          +
+        </button>
+        <button onClick={() => list.length > 0 && setList(list.slice(0, list.length - 1))}>
+          -
+        </button>
+      </span>
+      <ul>
+        {renderedList(list)}
+      </ul>
+    </div>
   );
 }
 
