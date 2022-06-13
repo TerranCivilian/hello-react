9033489be1c9eb904ed121344511cf0279790557
TerranCivilian <63529094+TerranCivilian@users.noreply.github.com>
2022-06-13 18:43:43 -0400

    remove some unnecessary things

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+const renderedList = list => list.map(i => <li>{i}</li>);
+
+function App() {
+  const [list, setList] = useState([1,2,3]);
+
+  return (
+    <div>
+      <span>
+        <button onClick={() => list.length > 0 ? setList([...list, list[list.length-1] + 1]) : setList([1])}>
+          +
+        </button>
+        <button onClick={() => list.length > 0 && setList(list.slice(0, list.length - 1))}>
+          -
+        </button>
+      </span>
+      <ul>
+        {renderedList(list)}
+      </ul>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
